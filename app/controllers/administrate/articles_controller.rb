9bb2741dab5a1767e7ba80fb01b5812af894ee5d
Fernando Vieira <fernandovieira.32@hotmail.com>
2025-02-23 18:26:57 -0300

    implementation basic devise

--- app/controllers/administrate/articles_controller.rb
+++ app/controllers/administrate/articles_controller.rb
@@ -2,6 +2,7 @@
 
 module Administrate
   class ArticlesController < ApplicationController
+    before_action :authenticate_admin!
     before_action :set_article, only: %i[show edit update destroy]
 
     # GET /articles or /articles.json
