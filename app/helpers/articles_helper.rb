bb1cda5c7e5341d735e7cdab61c6674b6a9d7e73
Fernando Vieira <fernandovieira.32@hotmail.com>
2025-04-13 19:07:44 -0300

    feat(class_25) ajust

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,4 @@
 # frozen_string_literal: true
 
 module ArticlesHelper
-  def cover_image(article)
-    article.cover_image.attached? ? article.cover_image.variant(:cover) : 'posts/post1.jpg'
-  end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module WelcomeHelper
+  def cover_image(article)
+    article.cover_image.attached? ? article.cover_image.variant(:cover) : 'posts/post1.jpg'
+  end
 end
