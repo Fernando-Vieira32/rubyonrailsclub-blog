20fe2232e09a704f66a4ce2545e1e6c4efad1259
Fernando Vieira <fernandovieira.32@hotmail.com>
2025-04-27 09:34:57 -0300

    feat(ajust_mensages) translate mensage

--- app/controllers/administrate/articles_controller.rb
+++ app/controllers/administrate/articles_controller.rb
@@ -6,29 +6,24 @@ module Administrate
     before_action :set_article, only: %i[destroy_cover_image show edit update destroy]
     layout 'administrate'
 
-    # GET /articles or /articles.json
     def index
       @articles = Article.all
     end
 
-    # GET /articles/1 or /articles/1.json
     def show; end
 
-    # GET /articles/new
     def new
       @article = Article.new
     end
 
-    # GET /articles/1/edit
     def edit; end
 
-    # POST /articles or /articles.json
     def create
       @article = Article.new(article_params)
 
       respond_to do |format|
         if @article.save
-          format.html { redirect_to [:administrate, @article], notice: 'Article was successfully created.' }
+          format.html { redirect_to [:administrate, @article], notice: 'Artigo criado com sucesso .' }
           format.json { render :show, status: :created, location: @article }
         else
           format.html { render :new, status: :unprocessable_entity }
@@ -37,11 +32,10 @@ module Administrate
       end
     end
 
-    # PATCH/PUT /articles/1 or /articles/1.json
     def update
       respond_to do |format|
         if @article.update(article_params)
-          format.html { redirect_to [:administrate, @article], notice: 'Article was successfully updated.' }
+          format.html { redirect_to [:administrate, @article], notice: 'Artigo atualizado com sucesso.' }
           format.json { render :show, status: :ok, location: @article }
         else
           format.html { render :edit, status: :unprocessable_entity }
@@ -50,13 +44,12 @@ module Administrate
       end
     end
 
-    # DELETE /articles/1 or /articles/1.json
     def destroy
       @article.destroy!
 
       respond_to do |format|
         format.html do
-          redirect_to administrate_articles_path, status: :see_other, notice: 'Article was successfully destroyed.'
+          redirect_to administrate_articles_path, status: :see_other, notice: 'Artigo excluído com sucesso.'
         end
         format.json { head :no_content }
       end
@@ -71,12 +64,10 @@ module Administrate
 
     private
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.friendly.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def article_params
       params.require(:article).permit(:title, :body, :cover_image)
     end
