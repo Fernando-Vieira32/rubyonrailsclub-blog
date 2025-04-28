5b32ad50590452f92b66565cc83df55aef9399fd
Fernando Vieira <fernandovieira.32@hotmail.com>
2025-04-28 10:23:12 -0300

    feat(class_45) add caterories

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,9 @@
 class Article < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  belongs_to :category
+
   has_one_attached :cover_image do |attachable| # this is use  image processing
     attachable.variant :thumb, resize_to_limit: [325, 205]
     attachable.variant :cover, resize_to_limit: [200, 200]
